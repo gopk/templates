12c66b386b55a4ff99f3356c2c09ead314cc5596
Dmitry Ponomarev <demdxx@gmail.com>
2016-07-06 18:10:00 +0300

    replace writer
    replace http.ResponseWriter to io.Writer

--- global.go
+++ global.go
@@ -6,48 +6,48 @@
 package templates
 
 import (
-  "html/template"
-  "net/http"
+	"html/template"
+	"io"
 )
 
 var (
-  GlobalRender *TemplateRender
+	GlobalRender *TemplateRender
 )
 
 func GetGlobalRender() *TemplateRender {
-  if nil == GlobalRender {
-    GlobalRender = MakeRender("", "", true)
-  }
-  return GlobalRender
+	if nil == GlobalRender {
+		GlobalRender = MakeRender("", "", true)
+	}
+	return GlobalRender
 }
 
 func InitGlobalRender(path, postfix string, enabledCache bool) *TemplateRender {
-  if nil == GlobalRender {
-    GlobalRender = MakeRender(path, postfix, enabledCache)
-  } else {
-    GlobalRender.path = path
-    GlobalRender.postfix = postfix
-    GlobalRender.CacheEnabled = enabledCache
-  }
-  return GlobalRender
+	if nil == GlobalRender {
+		GlobalRender = MakeRender(path, postfix, enabledCache)
+	} else {
+		GlobalRender.path = path
+		GlobalRender.postfix = postfix
+		GlobalRender.CacheEnabled = enabledCache
+	}
+	return GlobalRender
 }
 
 func Func(key string, value interface{}) {
-  GetGlobalRender().Func(key, value)
+	GetGlobalRender().Func(key, value)
 }
 
 func RegisterHandler(code int, handler RseponseHandler) *TemplateRender {
-  return GetGlobalRender().RegisterHandler(code, handler)
+	return GetGlobalRender().RegisterHandler(code, handler)
 }
 
 func Template(templates ...string) (*template.Template, error) {
-  return GetGlobalRender().Template(templates...)
+	return GetGlobalRender().Template(templates...)
 }
 
-func Render(w http.ResponseWriter, params map[string]interface{}, temps ...string) error {
-  return GetGlobalRender().Render(w, params, temps...)
+func Render(w io.Writer, params map[string]interface{}, temps ...string) error {
+	return GetGlobalRender().Render(w, params, temps...)
 }
 
 func RenderResponse(resp *HttpResponse) error {
-  return GetGlobalRender().RenderResponse(resp)
+	return GetGlobalRender().RenderResponse(resp)
 }
--- response.go
+++ response.go
@@ -6,58 +6,58 @@
 package templates
 
 import (
-  "net/http"
+	"net/http"
 )
 
 type HttpResponseHandler func(w http.ResponseWriter, r *http.Request) *HttpResponse
 
 type HttpResponse struct {
-  Request  *http.Request
-  Writer   http.ResponseWriter
-  Template string
-  Context  map[string]interface{}
-  Error    error
-  Code     int
+	Request  *http.Request
+	Writer   http.ResponseWriter
+	Template string
+	Context  map[string]interface{}
+	Error    error
+	Code     int
 }
 
 func Response(code int, template string, ctx map[string]interface{}) *HttpResponse {
-  return &HttpResponse{Code: code, Template: template, Context: ctx}
+	return &HttpResponse{Code: code, Template: template, Context: ctx}
 }
 
 func (resp *HttpResponse) SetBase(w http.ResponseWriter, r *http.Request) *HttpResponse {
-  resp.Writer = w
-  resp.Request = r
-  return resp
+	resp.Writer = w
+	resp.Request = r
+	return resp
 }
 
 func (resp *HttpResponse) UpdateContext(ctx map[string]interface{}) *HttpResponse {
-  if nil == resp.Context {
-    resp.Context = ctx
-  } else if nil != ctx {
-    for k, v := range resp.Context {
-      resp.Context[k] = v
-    }
-  }
-  return resp
+	if nil == resp.Context {
+		resp.Context = ctx
+	} else if nil != ctx {
+		for k, v := range resp.Context {
+			resp.Context[k] = v
+		}
+	}
+	return resp
 }
 
 func (resp *HttpResponse) SetContext(ctx map[string]interface{}) *HttpResponse {
-  resp.Context = ctx
-  return resp
+	resp.Context = ctx
+	return resp
 }
 
 func HttpHandler(render *TemplateRender, f HttpResponseHandler) http.HandlerFunc {
-  if nil == render {
-    render = GlobalRender
-  }
-  return func(w http.ResponseWriter, r *http.Request) {
-    resp := f(w, r)
-    if nil != resp {
-      resp.Writer = w
-      resp.Request = r
-      render.RenderResponse(resp)
-    } else {
-      http.Error(w, "Invalid http response", http.StatusInternalServerError)
-    }
-  }
+	if nil == render {
+		render = GlobalRender
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		resp := f(w, r)
+		if nil != resp {
+			resp.Writer = w
+			resp.Request = r
+			render.RenderResponse(resp)
+		} else {
+			http.Error(w, "Invalid http response", http.StatusInternalServerError)
+		}
+	}
 }
